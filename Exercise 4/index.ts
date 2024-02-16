0308e332bfe97ede08ab44fedee18b41fdd70f28
Ahmed Yaqoob Dhedhi <99260763+HamCraft@users.noreply.github.com>
2024-02-16 17:48:17 +0500

    Add files via upload

---
+++ Exercise 4/index.ts
@@ -0,0 +1,4 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+
+console.log('Naruto once said, “SOMEBODY TOLD ME I’M A FAILURE, I’LL PROVE THEM WRONG.”')
